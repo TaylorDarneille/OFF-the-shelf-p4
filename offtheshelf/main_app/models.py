d037b565b866654a045f5ca1a81ef4ec1d318236
Michaudsr <68039781+Michaudsr@users.noreply.github.com>
2020-09-26 19:51:52 -0400

    [models] stubbed out wishlist and comment models

--- offtheshelf/main_app/models.py
+++ offtheshelf/main_app/models.py
@@ -5,7 +5,15 @@ from django.contrib.auth.models import User
 
 # class Wishlist(models.Model):
 #     user = models.ForeignKey(User, on_delete=models.CASCADE)
-#     # book = 
+#     book_id = models.IntegerField()
 #     title = models.CharField(max_length=100)
-    # def __str__(self):
-    #     return self.name
+#     def __str__(self):
+#         return self.name
+
+# class Comment(models.Model):
+#     content = models.CharField(max_length=250)
+#     book_id = models.IntegerField()
+#     user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+#     def str(self):
+#         return self.itemName
