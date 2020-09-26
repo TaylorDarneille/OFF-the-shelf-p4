f49f2516b9a3639d417b37b250dd91345b0eaa68
Lizz <lizwesterband@gmail.com>
2020-09-26 11:50:41 -0700

    .env should stay hidden

--- offtheshelf/main_app/urls.py
+++ offtheshelf/main_app/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 from . import views
 
-# urlpatterns = [
-#     path('', views.index, name='index'),
+urlpatterns = [
+    path('', views.index, name='index'),
 #     path('search_results/', views.search_results, name='search_results'),
 #     path('signup/', views.signup_view, name='signup'),
 #     path('login/', views.login_view, name='login'),
 #     path('logout/', views.logout_view, name='logout'),
 #     path('user/<username>/', views.profile, name='profile'),
 #     path('book_info/', views.book_show, name="book_info"),
-# ]
+]
