097423b95a23cfc1f1bee9dfcf1bd8a166455f81
Lizz <lizwesterband@gmail.com>
2020-09-28 11:05:49 -0700

    add changes to models to display img on profile

--- offtheshelf/main_app/admin.py
+++ offtheshelf/main_app/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Wishlist, Comment
+from .models import Wishlist, Comment, LetsTry
 
 
 # Register your models here.
 admin.site.register(Wishlist)
 admin.site.register(Comment)
+admin.site.register(LetsTry)
--- offtheshelf/main_app/models.py
+++ offtheshelf/main_app/models.py
@@ -7,6 +7,7 @@ class Wishlist(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     book_id = models.IntegerField()
     title = models.CharField(max_length=100)
+    img_url = models.CharField(max_length=150)
     def __str__(self):
         return self.title
 
@@ -16,4 +17,11 @@ class Comment(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def str(self):
+        return self.content
+
+class LetsTry(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = content = models.CharField(max_length=250)
+    
+    def __str__(self):
         return self.content
