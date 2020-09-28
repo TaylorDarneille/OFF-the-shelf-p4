12ea3d1223d9058c6e2bd57bf4cebacc76324ea1
maestronick1 <maestronicknrp@gmail.com>
2020-09-28 12:20:41 -0700

    add audio tag

--- offtheshelf/offtheshelf/settings.py
+++ offtheshelf/offtheshelf/settings.py
@@ -16,6 +16,9 @@ from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+MEDIA_ROOT=os.path.join(BASE_DIR,"songdir")
+MEDIA_URL='/media/'
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
