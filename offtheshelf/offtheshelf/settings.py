e6966817813784fe12d7cd50d77f4f9feaa077fc
maestronick1 <maestronicknrp@gmail.com>
2020-09-29 16:26:08 -0700

    crispy

--- offtheshelf/offtheshelf/settings.py
+++ offtheshelf/offtheshelf/settings.py
@@ -42,6 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
+
 ]
 
 MIDDLEWARE = [
@@ -121,5 +123,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
+
+CRISPY_TEMPLATE_PACK = 'bootstrap3'
+
 
 STATIC_URL = '/static/'
